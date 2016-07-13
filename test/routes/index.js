6040106dabf6017f9ec341576373fb723cf68298
arunabaijal <aruna11baijal@gmail.com>
2016-07-13 17:02:26 +0530

    Update index.js

--- test/routes/index.js
+++ test/routes/index.js
@@ -20,37 +20,58 @@ db = new Db('clientdb', server);
 db.open(function(err, db) {
     if(!err) {
         console.log("Connected to 'clientdb' database");
-        db.collection('index', {strict:true}, function(err, collection) {
+        db.collection('clients', {strict:true}, function(err, collection) {
             if (err) {
                 console.log("The 'clients' collection doesn't exist. Creating it with sample data...");
-                populateDB();
+                //populateDB();
+            }
+        });
+        db.collection('metadata', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'metadata' collection doesn't exist. Creating it with sample data...");
+                //populateDB();
             }
         });
     }
 });
 
-//var clientdb = mongoose.model('Post');
-// var active = mongoose.model('active');
-// var hostingtype=mongoose.model('hostingtype');
-// var industry=mongoose.model('industry');
-// var backend=mongoose.model('backend');
-// var geography=mongoose.model('geography');
-// var region=mongoose.model('region');
-// var edi=mongoose.model('edi');
-// var location=mongoose.model('location');
-// var solution=mongoose.model('solution');
-// var rtc=mongoose.model('rtc');
-// var lifecyle=mongoose.model('lifecyle');
-// var fromdate=mongoose.model('from');
-// var todate=mongoose.model('todate');
-// var thirdparty=mongoose.model('thirdparty');
-// var icn=mongoose.model('icn');
-
 router.addClient = function(req, res) {
     var client = req.body;
-    console.log('Adding client: ' + JSON.stringify(client));
+    if(collection.find({name : req.body.name}).toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+          } else if (result.length) {
+            return true;
+          } else {
+            return false;
+          }
+        })
+      )
+    {
+      res.send('Client already exists!');
+      res.render('adminstart');
+    }
+    else
+    {
+        console.log('Adding client: ' + JSON.stringify(client));
+        db.collection('clients', function(err, collection) {
+            collection.insert(client, {safe:true}, function(err, result) {
+                if (err) {
+                    res.send({'error':'An error has occurred'});
+                } else {
+                    console.log('Success: ' + JSON.stringify(client));
+                    res.render('adminstart');
+                }
+            });
+        });
+    }
+  }
+
+router.updateClient = function(req, res) {
+    var client = req.body;
+    console.log('Updating client: ' + req.body.name);
     db.collection('clients', function(err, collection) {
-        collection.insert(client, {safe:true}, function(err, result) {
+        collection.update({name : req.body.name}, client, {safe:true}, function(err, result) {
             if (err) {
                 res.send({'error':'An error has occurred'});
             } else {
@@ -61,46 +82,172 @@ router.addClient = function(req, res) {
     });
 }
 
-exports.searchClient = function(req, res) {
-    var name = req.params.name;
-    console.log('Retrieving client: ' + name);
-    db.collection('index', function(err, collection) {
-        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-            res.send(client);
-        });
-    });
+var collection = db.collection('clients');
+router.searchClient = function(req, res) {
+   var s = {};
+  if (req.body.name !== "")
+  {
+    var str = req.body.name;
+    s['name'] = str;
+  }
+  if (req.body.Active == "active" || req.body.Active == "inactive")
+  {
+    var str = req.body.Active;
+    s['active'] = str;
+  }
+  if (req.body.hostingtype == "onpremise" || req.body.hostingtype == "saas")
+  {
+    var str = req.body.hostingtype;
+    s['hostingtype'] = str;
+  }
+  if (req.body.industry !== "")
+  {
+    var str = req.body.industry;
+    s['industry'] = str;
+  }
+  if (req.body.geography !== "")
+  {
+    var str = req.body.geography;
+    s['geography'] = str;
+  }
+  if (req.body.region !== "")
+  {
+    var str = req.body.region;
+    s['region'] = str;
+  }
+  if (req.body.location !== "")
+  {
+    var str = req.body.location;
+    s['location'] = str;
+  }
+  if (req.body.edi !== "")
+  {
+    var str = req.body.edi;
+    s['edi'] = str;
+  }
+  if (req.body.lifecycle == "implementation" || req.body.lifecycle == "migration" || req.body.lifecycle == "product enhancement/support")
+  {
+    var str = req.body.lifecycle;
+    s['lifecycle'] = str;
+  }
+  if (req.body.fromdate !== "")
+  {
+    var str = req.body.fromdate;
+    s['fromdate'] = str;
+  }
+  if (req.body.todate !== "")
+  {
+    var str = req.body.todate;
+    s['todate'] = str;
+  }
+  if (req.body.icn !== "")
+  {
+    var str = req.body.icn;
+    s['icn'] = str;
+  }
+  console.log(s);
+  //var resjson = JSON.parse(s);
+  collection.find(s).toArray(function (err, result) {
+      if (err) {
+        console.log(err);
+      } else if (result.length) {
+        console.log('Found:', result);
+        res.json(result);
+      } else {
+        console.log('No document(s) found with defined "find" criteria!');
+        res.render('failsearch')
+      }
+  });
 };
 
-/*router.post('/posts', function(req, res, next) {
-  var name = new name(req.body);
-
-  name.save(function(err, name){
-    if(err){ return next(err); }
-
-    res.json(name);
+router.searchClientAdmin = function(req, res) {
+   var s = {};
+  if (req.body.name !== "")
+  {
+    var str = req.body.name;
+    s['name'] = str;
+  }
+  if (req.body.Active == "active" || req.body.Active == "inactive")
+  {
+    var str = req.body.Active;
+    s['active'] = str;
+  }
+  if (req.body.hostingtype == "onpremise" || req.body.hostingtype == "saas")
+  {
+    var str = req.body.hostingtype;
+    s['hostingtype'] = str;
+  }
+  if (req.body.industry !== "")
+  {
+    var str = req.body.industry;
+    s['industry'] = str;
+  }
+  if (req.body.geography !== "")
+  {
+    var str = req.body.geography;
+    s['geography'] = str;
+  }
+  if (req.body.region !== "")
+  {
+    var str = req.body.region;
+    s['region'] = str;
+  }
+  if (req.body.location !== "")
+  {
+    var str = req.body.location;
+    s['location'] = str;
+  }
+  if (req.body.edi !== "")
+  {
+    var str = req.body.edi;
+    s['edi'] = str;
+  }
+  if (req.body.lifecycle == "implementation" || req.body.lifecycle == "migration" || req.body.lifecycle == "product enhancement/support")
+  {
+    var str = req.body.lifecycle;
+    s['lifecycle'] = str;
+  }
+  if (req.body.fromdate !== "")
+  {
+    var str = req.body.fromdate;
+    s['fromdate'] = str;
+  }
+  if (req.body.todate !== "")
+  {
+    var str = req.body.todate;
+    s['todate'] = str;
+  }
+  if (req.body.icn !== "")
+  {
+    var str = req.body.icn;
+    s['icn'] = str;
+  }
+  console.log(s);
+  //var resjson = JSON.parse(s);
+  collection.find(s).toArray(function (err, result) {
+      if (err) {
+        console.log(err);
+      } else if (result.length) {
+        console.log('Found:', result);
+        res.json(result);
+      } else {
+        console.log('No document(s) found with defined "find" criteria!');
+        res.render('failsearchadmin')
+      }
   });
-});
+};
 
-router.get('/posts', function(req, res, next) {
-  Post.find(function(err, posts){
-    if(err){ return next(err); }
-
-    res.json(posts);
-  });
-});
-
-
-*/router.get('/user', function(req, res, next) {
+router.get('/user', function(req, res, next) {
   res.render('user');
 });
 module.exports = router;
 
 
-var populateDB = function() {
+/*var populateDB = function() {
 
     var clients = [
     {
-        name: "Client_sample",
+        name: "Sample",
         active : "active",
         hostingtype:"hostingtype",
         industry:"industry",
@@ -122,4 +269,4 @@ var populateDB = function() {
         collection.insert(clients, {safe:true}, function(err, result) {});
     });
 
-};
+};*/
