3f2125a52046f373996087e0b4a8a2a7724bbe96
arunabaijal <aruna11baijal@gmail.com>
2016-07-13 17:28:52 +0530

    Update users.js

--- test/routes/users.js
+++ test/routes/users.js
@@ -1,12 +1,10 @@
-// var express = require('express');
-// var router = express.Router();
-// var client = require('./index');
+var express = require('express');
+var router = express.Router();
+var client = require('./index');
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
 
-// router.post('/index', client.addClient);
+router.get('/', function(req, res, next) {
+  res.render('user');
+});
 
-// module.exports = router;
+module.exports = router;
